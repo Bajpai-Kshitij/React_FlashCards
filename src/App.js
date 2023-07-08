87bd9604890c0c41654cf8be0f75792399bf03b1
Rapheal Jones <110284565+Bajpai-Kshitij@users.noreply.github.com>
2023-07-08 10:52:44 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  return (
+    <div className="flashcards">
+      {questions.map((elem) => (
+        <Flash {...elem} key={elem.id} />
+      ))}
+    </div>
+  );
+}
+
+function Flash({ question, answer }) {
+  const [isSelected, setSelected] = useState(false);
+
+  function handleClick() {
+    setSelected((key) => !key);
+    console.log("Clicked");
+  }
+  return (
+    <div>
+      <span onClick={handleClick}>{isSelected ? answer : question}</span>
     </div>
   );
 }

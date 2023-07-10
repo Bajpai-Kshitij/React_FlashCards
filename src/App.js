715fe29dcc411babb837c7170081bef1bc357750
Bajpai-Kshitij <hbajpaikshitij@gmail.com>
2023-07-10 07:16:07 +0530

    conditional slider added

--- src/App.js
+++ src/App.js
@@ -44,25 +44,54 @@ const questions = [
 ];
 
 function FlashCards() {
+  const [isSelected,setSelected] = useState(null)
+  function handleClick(selectedId){
+    setSelected(selectedId === isSelected ? null : selectedId)
+  }
   return (
     <div className="flashcards">
-      {questions.map((elem) => (
-        <Flash {...elem} key={elem.id} />
-      ))}
+      {questions.map((elem) => {
+        const selectField = isSelected === elem.id;
+        return (
+          <div key={elem.id} onClick={() => handleClick(elem.id)} className={selectField ? "selected":""}>
+            <p >{selectField ? elem.answer : elem.question}</p>
+          </div>
+        )
+    })}
+    <ConditionalSlider  />
     </div>
   );
 }
 
-function Flash({ question, answer }) {
-  const [isSelected, setSelected] = useState(false);
 
-  function handleClick() {
-    setSelected((key) => !key);
-    console.log("Clicked");
-  }
+
+
+const countries = [
+  { name: "India", value: "IN", cities: ["Kanpur", "Delhi"] },
+  { name: "Usa", value: "US", cities: ["New York", "California"] },
+  { name: "France", value: "FR", cities: ["Paris"] }
+];
+
+function ConditionalSlider() {
+  const [country, setCountry] = useState("");
   return (
-    <div>
-      <span onClick={handleClick}>{isSelected ? answer : question}</span>
+    <div className="App">
+      <select onChange={(e) => setCountry(e.target.value)}>
+        <option value="">Choose...</option>
+        {countries.map((item) => <option value={item.name}>{item.name}</option> )}
+      </select>
+
+      {country && (
+        <select>
+          {countries
+            .filter((item) => item.name === country)
+            .flatMap((item) => {
+              return item.cities.map((city) => (
+                <option value={city}>{city}</option>
+              ));
+            })}
+        </select>
+      )}
     </div>
   );
 }
